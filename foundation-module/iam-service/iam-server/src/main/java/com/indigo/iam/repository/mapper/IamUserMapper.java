195a4532619737b1e891a7631b55a19813a5b23d
shixiecheng <shixiecheng@skyisz.com>
2025-09-22 17:31:21 +0800

    feat:修复bug

--- foundation-module/iam-service/iam-server/src/main/java/com/indigo/iam/repository/mapper/IamUserMapper.java
+++ foundation-module/iam-service/iam-server/src/main/java/com/indigo/iam/repository/mapper/IamUserMapper.java
@@ -1,6 +1,7 @@
 package com.indigo.iam.repository.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.indigo.databases.mapper.EnhancedVoMapper;
 import com.indigo.iam.repository.entity.Users;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,5 +10,5 @@ import org.apache.ibatis.annotations.Mapper;
  * @date 2025/07/22 16:15
  **/
 @Mapper
-public interface IamUserMapper extends BaseMapper<Users> {
+public interface IamUserMapper extends BaseMapper<Users>, EnhancedVoMapper<Users, Object> {
 }
